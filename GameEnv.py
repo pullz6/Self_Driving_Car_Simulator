11ed84327c6f6c769d05a97ab6c6cd26a55c5475
Sara PG <39550635+pullz6@users.noreply.github.com>
2024-10-29 20:31:46 +0000

    Add files via upload

--- GameEnv.py
+++ GameEnv.py
@@ -1,12 +1,14 @@
 import pyglet
-window = pyglet.window.Window()
-label = pyglet.text.Label('Hello, world',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+from pyglet import shapes
+
+window = pyglet.window.Window(width=1280, height = 720, caption = 'Car Simulator')
+circle = shapes.Circle(x=700,y=150,radius=100, color=(50,225,30))
+#label = pyglet.text.Label('Car goes Vroom Vroom',font_name='Times New Roman',font_size=36,x=window.width//2, y=window.height//2, anchor_x='center', anchor_y='center')
+
+#Registering the function
 @window.event
 def on_draw():
     window.clear()
-    label.draw()
+    circle.draw()
+    
 pyglet.app.run()
