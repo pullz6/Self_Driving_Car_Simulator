1a603cddd7921637f5c795ac6bdfe71aabd19633
Sara PG <39550635+pullz6@users.noreply.github.com>
2024-11-03 23:03:41 +0000

    Few more track collision courses' adjustments

--- GameEnv.py
+++ GameEnv.py
@@ -15,17 +15,34 @@ imageHeight = 523
 track_sprite.width = imageWidth
 track_sprite.height = imageHeight
 
-#Creating the track boundary 
+#For the innner track
+#Creating the track boundary with circles for the edges 
+circle1_left = shapes.Circle(x=230,y=209,radius=91, color=(50,225,30))
+circle1_right = shapes.Circle(x=570,y=209,radius=91, color=(50,225,30))
 
-##First lets create the lines
-line_top1 = shapes.Line(230, 295, 570, 295, width=12)
-line_bottom1 = shapes.Line(230, 111, 570, 111, width=12, color=(200,20,20))
+##First lets create the lines o the track 
+line_top1 = shapes.Line(230, 295, 570, 295, width=11.5)
+line_bottom1 = shapes.Line(230, 123, 570, 123, width=11.5, color=(200,20,20))
+
+#For the outer track 
+#Creating the track boundary with circles for the edges 
+
+
+##First lets create the lines o the track 
+line_top2 = shapes.Line(200, 660, 610, 660, width=11.5)
+line_bottom2 = shapes.Line(200, 13.7, 610, 13.7, width=11.5, color=(200,20,20))
+
 
 #Registering the function
 @window.event
 def on_draw():
     track_sprite.draw()
     line_top1.draw()
+    circle1_left.draw()
+    circle1_right.draw()
     line_bottom1.draw()
+    line_top2.draw()
+    line_bottom2.draw()
+    
     
 pyglet.app.run()
