157cc4d42e2d85516652541955eb046d3be9816d
Sara PG <39550635+pullz6@users.noreply.github.com>
2024-11-05 23:50:19 +0000

    Add files via upload

--- GameEnv.py
+++ GameEnv.py
@@ -1,6 +1,7 @@
 import pyglet
 from pyglet import shapes
 from shapely.geometry import Polygon
+import math
 
 #Create the window
 window = pyglet.window.Window(width=800, height = 400, caption = 'Race Track')
@@ -14,7 +15,7 @@ imageWidth = 800
 imageHeight = 523
 track_sprite.width = imageWidth
 track_sprite.height = imageHeight
-RADIUS = 400
+
 #For the innner track
 #Creating the track boundary with circles for the edges 
 circle1_left = shapes.Circle(x=230,y=209,radius=91, color=(50,225,30))
@@ -25,21 +26,22 @@ line_top1 = shapes.Line(230, 295, 570, 295, width=11.5)
 line_bottom1 = shapes.Line(230, 123, 570, 123, width=11.5, color=(200,20,20))
 
 # Control points for the semicircle
-p0 = (imageWidth // 2 - RADIUS, imageHeight // 2)
-p1 = (imageWidth// 2 - RADIUS / 2, imageHeight // 2 + RADIUS)
-p2 = (imageWidth // 2 + RADIUS / 2, imageHeight // 2 + RADIUS)
-p3 = (imageWidth // 2 + RADIUS, imageHeight // 2)
+RADIUS = 185
+CENTER_X = 195
+CENTER_Y = 210
+start_angle = math.pi / 2 # Start angle in radians
+angle = math.pi
 
-# Create a Bézier curve shape
-bezier_curve = shapes.BezierCurve(p0, p1, p2, p3, color=(255, 0, 0))
+# Create an arc for the rounded edges
+arc = shapes.Arc(x=CENTER_X, y=CENTER_Y, radius = RADIUS, start_angle= start_angle , angle=angle, color=(255, 0, 0),segments=100,thickness = 11.5)
 
 #For the outer track 
 #Creating the track boundary with circles for the edges 
 
 
 ##First lets create the lines o the track 
-line_top2 = shapes.Line(200, 660, 610, 660, width=11.5)
-line_bottom2 = shapes.Line(200, 13.7, 610, 13.7, width=11.5, color=(200,20,20))
+line_top2 = shapes.Line(190, 660, 610, 660, width=11.5, color=(200,20,20))
+line_bottom2 = shapes.Line(190, 13.7, 610, 13.7, width=11.5, color=(200,20,20))
 
 
 #Registering the function
@@ -51,7 +53,7 @@ def on_draw():
     circle1_right.draw()
     line_bottom1.draw()
     line_top2.draw()
-    bbezier_curve.draw()
+    arc.draw()
     line_bottom2.draw()
     
     
