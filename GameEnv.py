ce34a3df00c55acc17aa064fea1c7f5bfb81be84
Sara PG <39550635+pullz6@users.noreply.github.com>
2024-11-06 19:55:17 +0000

    Collision Track done.

--- GameEnv.py
+++ GameEnv.py
@@ -22,7 +22,7 @@ circle1_left = shapes.Circle(x=230,y=209,radius=91, color=(50,225,30))
 circle1_right = shapes.Circle(x=570,y=209,radius=91, color=(50,225,30))
 
 ##First lets create the lines o the track 
-line_top1 = shapes.Line(230, 295, 570, 295, width=11.5)
+line_top1 = shapes.Line(230, 295, 570, 295, width=11.5, color=(200,20,20))
 line_bottom1 = shapes.Line(230, 123, 570, 123, width=11.5, color=(200,20,20))
 
 # Control points for the semicircle
@@ -33,27 +33,38 @@ start_angle = math.pi / 2 # Start angle in radians
 angle = math.pi
 
 # Create an arc for the rounded edges
-arc = shapes.Arc(x=CENTER_X, y=CENTER_Y, radius = RADIUS, start_angle= start_angle , angle=angle, color=(255, 0, 0),segments=100,thickness = 11.5)
+arc_1 = shapes.Arc(x=CENTER_X, y=CENTER_Y, radius = RADIUS, start_angle= start_angle , angle=angle, color=(255, 0, 0),segments=100,thickness = 11.5)
+
+RADIUS = 185
+CENTER_X = 600
+CENTER_Y = 210
+start_angle = 3 * math.pi / 2  # Start angle at 270 degrees (3π/2 radians)
+angle = math.pi
+
+# Create an arc for the rounded edges
+arc_2 = shapes.Arc(x=CENTER_X, y=CENTER_Y, radius = RADIUS, start_angle= start_angle , angle=angle, color=(255, 0, 0),segments=100,thickness = 11.5)
+
 
 #For the outer track 
-#Creating the track boundary with circles for the edges 
-
 
 ##First lets create the lines o the track 
-line_top2 = shapes.Line(190, 660, 610, 660, width=11.5, color=(200,20,20))
-line_bottom2 = shapes.Line(190, 13.7, 610, 13.7, width=11.5, color=(200,20,20))
+line_top2 = shapes.Line(190, 395, 610, 395, width=11.5, color=(200,20,20))
+line_bottom2 = shapes.Line(190, 14, 610, 13.7, width=11.5, color=(200,20,20))
 
 
 #Registering the function
 @window.event
 def on_draw():
     track_sprite.draw()
+    #Inner Track
     line_top1.draw()
     circle1_left.draw()
     circle1_right.draw()
     line_bottom1.draw()
+    #OuterTrack
     line_top2.draw()
-    arc.draw()
+    arc_1.draw()
+    arc_2.draw()
     line_bottom2.draw()
     
     
